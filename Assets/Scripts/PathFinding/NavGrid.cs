74d4b15aedc544e279d3adafb278c8b3490e7a9d
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-17 18:12:38 +0200

    Add description to NavGrid methods

--- Assets/Scripts/PathFinding/NavGrid.cs
+++ Assets/Scripts/PathFinding/NavGrid.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace Nav2D
 {
@@ -16,9 +15,12 @@ namespace Nav2D
                 + " enabling it in current state can lead to unknown behaviour")]
         public bool diagnonalMovement;
 
+        /// <summary>
+        /// Size of single cell in the navigation grid
+        /// </summary>
         public float CellSize { get; private set; }
+        public Vector2 Offset { get; private set; }
         private NodeType[,] nodes;
-        private Vector2Int offset;
         private Bounds bounds;
 
         public bool IsWithinNavGridBounds(Vector2 position)
@@ -26,56 +28,83 @@ namespace Nav2D
             return bounds.Contains(position);
         }
 
-        public void Init(int width, int height, Vector2Int offset, float cellSize = 1f)
+
+        /// <summary>
+        /// Initialize navigation grid with nodes of type <seealso cref="NodeType.free"/>,
+        /// use this method to change navigation grid parameters as size, offset or cellSize.
+        /// </summary>
+        /// <param name="width">width of naviation grid</param>
+        /// <param name="height">height of naviagtion grid</param>
+        /// <param name="offset">naviagtion grid offset</param>
+        /// <param name="cellSize">size of single cell in the navigation grid</param>
+        public void Init(int width, int height, Vector2 offset, float cellSize = 1f)
         {
             width = Mathf.Max(0, width);
             height = Mathf.Max(0, height);
 
             nodes = new NodeType[width, height];
-            this.offset = offset;
+            Offset = offset;
             CellSize = cellSize;
             bounds = new Bounds(Vector3.zero, Vector3.zero);
             bounds.Encapsulate(IndexToPosition(new Vector2Int(0, 0)) - (Vector2.one * cellSize / 2));
             bounds.Encapsulate(IndexToPosition(new Vector2Int(width - 1, height - 1)) + (Vector2.one * cellSize / 2));
         }
 
-        public void SetNode(int x, int y, NodeType node)
+        /// <summary>
+        /// Sets node type for node with specify index
+        /// </summary>
+        /// <param name="nodeType">type of the node</param>
+        public void SetNode(int x, int y, NodeType nodeType)
         {
-            if (IsValideIndex(x, y))
-            {
-                nodes[x, y] = node;
-            }
-            else
-            {
-                Debug.LogError($"index {x}:{y} is invalide");
-            }
+            nodes[x, y] = nodeType;
         }
 
+        /// <summary>
+        /// Checks if node with index x, y is of specify type. Returns false if index excede range
+        /// </summary>
         public bool IsNodeOfType(int x,int y,NodeType nodeType)
         {
             return IsValideIndex(x, y) && nodes[x, y] == nodeType;
         }
 
+
+        /// <summary>
+        /// return type of node with index x, y. Returns <seealso cref="NodeType.free"/> if index exceed range
+        /// </summary>
         public NodeType GetNodeType(int x, int y)
         {
             return IsValideIndex(x, y) ? nodes[x, y] : NodeType.free;
         }
 
+        /// <summary>
+        /// Checks whether node is of type <seealso cref="NodeType.free"/>, returns true if index exceed range
+        /// </summary>
         public bool IsFreeNode(int x, int y)
         {
             return GetNodeType(x,y) == NodeType.free;
         }
 
+        /// <summary>
+        /// Checks whether node is grounded or not, returns false if index exceed range
+        /// </summary>
         public bool IsGroundedNode(int x,int y)
         {
             return GetNodeType(x, y - 1) == NodeType.wall;
         }
 
+        /// <summary>
+        /// Checks whether node is grounded or not, returns false if index exceed range
+        /// </summary>
         public bool IsCelingNode(int x,int y)
         {
             return GetNodeType(x, y + 1) == NodeType.wall;
         }
 
+        /// <summary>
+        /// Get neighbors for specify node index
+        /// </summary>
+        /// <param name="indexVector">node index</param>
+        /// <returns>list of neighbors indexes</returns>
         public List<Vector2Int> GetNeightbors(Vector2Int indexVector)
         {
             var output = new List<Vector2Int>();
@@ -98,29 +127,45 @@ namespace Nav2D
             return output;
         }
 
-
+        /// <summary>
+        /// Convert a node index to world position
+        /// </summary>
+        /// <param name="indexVector">node index</param>
+        /// <returns>world position of node center</returns>
+        /// <remarks>This method return center position of the node, whitch is determinate by <seealso cref="CellSize"/></remarks>
         public Vector2 IndexToPosition(Vector2Int indexVector)
         {
-            var output = CellSize * (Vector2)indexVector + Vector2.one * (CellSize / 2) + offset;
+            var output = CellSize * (Vector2)indexVector + Vector2.one * (CellSize / 2) + Offset;
             
             return output;
         }
 
+        /// <summary>
+        /// Convert a world position to node index
+        /// </summary>
+        /// <param name="position">world position</param>
+        /// <returns>node index</returns>
         public Vector2Int PositionToIndex(Vector2 position)
         {
             if (IsWithinNavGridBounds(position) == false) return Vector2Int.left;
-            var distance = position - offset;
+            var distance = position - Offset;
             var vectorIndex = new Vector2Int((int)(distance.x / CellSize), (int)(distance.y / CellSize));
             vectorIndex.x = Mathf.Abs(vectorIndex.x);
             vectorIndex.y = Mathf.Abs(vectorIndex.y);
             return vectorIndex;
         }
 
+        /// <summary>
+        /// Returns closes point to specify position that is within navigation grid bounds
+        /// </summary>
         public Vector2 GetClosesPoint(Vector2 position)
         {
             return bounds.ClosestPoint(position);
         }
 
+        /// <summary>
+        /// Checks if specified index does not exceed range
+        /// </summary>
         public bool IsValideIndex(int x, int y)
         {
             return x >= 0 && x < nodes.GetLength(0) && y >= 0 && y < nodes.GetLength(1);
