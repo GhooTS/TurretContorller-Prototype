bfbebff331a146d213006d171e120c512825a33f
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-24 20:21:04 +0200

    Adapt ShootActionSelector to new selection

--- Assets/Scripts/Action System/ShootActionSelector.cs
+++ Assets/Scripts/Action System/ShootActionSelector.cs
@@ -6,29 +6,38 @@ using UnityEngine.InputSystem;
 //AFTER ADDING THE UNIT SELECTION SYSTEM, THE CLASS WILL BE REFACTORED
 public class ShootActionSelector : MonoBehaviour
 {
-    public Unit unit;
+    public LayerMask obstractionLayer;
+    private Unit unit;
     private ShootActionController controller;
     public LineRenderer bulletTrajectoryDisplay;
     private Vector2 target;
     private bool targetSelection = false;
     private float distanceToSpawnpoint = 0;
+    private Selection selection;
     new private Camera camera;
 
 
     private void Start()
     {
-        controller = unit.GetComponent<ShootActionController>();
-        distanceToSpawnpoint = (controller.rotationPoint.position - controller.bulletSpawnPoint.position).magnitude;
+        selection = FindObjectOfType<Selection>();
         camera = Camera.main;
     }
 
     private void Update()
     {
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ActiveTargetSelection();
+        }
+
         if (targetSelection)
         {
             target = camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector2 spawnPosition = controller.rotationPoint.position;
-            Vector2 hitPosition = Physics2D.Raycast(spawnPosition, target - (Vector2)spawnPosition).point;
+            Vector2 hitPosition;
+            var hit = Physics2D.Raycast(spawnPosition, target - spawnPosition, 20f, obstractionLayer);
+            hitPosition = hit.collider == null ? spawnPosition + (target - spawnPosition).normalized * 20f  : hit.point;
             Vector2 direction = (hitPosition - spawnPosition).normalized;
             spawnPosition += direction * distanceToSpawnpoint;
             bulletTrajectoryDisplay.SetPosition(0,spawnPosition);
@@ -38,7 +47,15 @@ public class ShootActionSelector : MonoBehaviour
             {
                 targetSelection = false;
                 AddAction();
+                selection.lockSelection = false;
             }
+
+            if (Mouse.current.rightButton.wasPressedThisFrame)
+            {
+                targetSelection = false;
+                selection.lockSelection = false;
+            }
+
         }
     }
     public void AddAction()
@@ -66,7 +83,12 @@ public class ShootActionSelector : MonoBehaviour
 
     public void ActiveTargetSelection()
     {
-        bulletTrajectoryDisplay.positionCount = 2;
-        targetSelection = true;
+        if (selection.HasSelection() && selection.selected.TryGetComponent(out unit) && selection.selected.TryGetComponent(out controller))
+        {
+            bulletTrajectoryDisplay.positionCount = 2;
+            distanceToSpawnpoint = (controller.rotationPoint.position - controller.bulletSpawnPoint.position).magnitude;
+            targetSelection = true;
+            selection.lockSelection = true;
+        }
     }
 }
