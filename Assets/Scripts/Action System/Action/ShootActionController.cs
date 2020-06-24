dfeb80d5bc842f86c93e78f38faaea6097ffaa30
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-24 20:40:57 +0200

    Add bullet spread and possibility to shoot more than one bullet

--- Assets/Scripts/Action System/Action/ShootActionController.cs
+++ Assets/Scripts/Action System/Action/ShootActionController.cs
@@ -6,22 +6,34 @@ public class ShootActionController : ActionController
     public Bullet bulletPrefab;
     public Transform bulletSpawnPoint;
     public Transform rotationPoint;
-    public float rotationSpeed = 10f;
-    private Bullet instance;
+    public float rotationSpeed = 10f; // How fast turret rotate
+    public float fireRate = 0.2f; // How fast turret shoots
+    public float bulletSpread = 12f; // in degrees
+    public int numberOfShoots = 3; // How much bullets turret shoot in one action
+    
     private float rotateTo;
-    private Vector2 currentTarget;
+    private Vector2 currentTarget; 
+    private float nextShootTime = 0;
+    private int bulletLeft = 0;
 
     private void Update()
     {
         if(active)
         {
-            var rotation = rotationPoint.localRotation.eulerAngles;
-            rotation.z = Mathf.LerpAngle(rotation.z,rotateTo, rotationSpeed * Time.deltaTime);
-            rotationPoint.localRotation = Quaternion.Euler(rotation);
-            if((int)rotation.z == (int)rotateTo)
+            if ((int)rotationPoint.localRotation.eulerAngles.z != (int)rotateTo)
+            {
+                var rotation = rotationPoint.localRotation.eulerAngles;
+                rotation.z = Mathf.LerpAngle(rotation.z,rotateTo, rotationSpeed * Time.deltaTime);
+                rotationPoint.localRotation = Quaternion.Euler(rotation);
+            }
+            else if(nextShootTime < Time.time)
             {
                 Shoot();
-                active = false;
+
+                if (bulletLeft <= 0)
+                {
+                    active = false;
+                }
             }
         }
     }
@@ -33,19 +45,28 @@ public class ShootActionController : ActionController
         rotateTo = rotationPoint.GetLookAtAngle(currentTarget);
         rotateTo = rotateTo < 0 ? 360 - Mathf.Abs(rotateTo) : rotateTo;
         active = true;
+        nextShootTime = 0;
+        bulletLeft = numberOfShoots;
     }
 
     public override bool HasFinshed()
     {
-        return active == false && instance == null;
+        return active == false;
     }
 
 
     private void Shoot()
     {
-        var direction = (currentTarget - (Vector2)bulletSpawnPoint.position).normalized;
-        instance = Instantiate(bulletPrefab, bulletSpawnPoint.position, rotationPoint.rotation);
+        var direction = Vector2.zero;
+        var newShootAngle = rotationPoint.eulerAngles.z + Random.Range(0f, bulletSpread) * (Random.value < .50f ? -1 : 1);
+        direction.x = Mathf.Cos(newShootAngle * Mathf.Deg2Rad);
+        direction.y = Mathf.Sin(newShootAngle * Mathf.Deg2Rad);
+        Debug.Log($"{rotateTo} :: {newShootAngle}");
+
+        var instance = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.Euler(0,0,newShootAngle));
         instance.Shoot(direction);
+        bulletLeft--;
+        nextShootTime = Time.time + fireRate;
     }
 }
 
