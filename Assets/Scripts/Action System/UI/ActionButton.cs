a6550b50cd27b3de3735870728acfa66ed808df6
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-26 23:10:00 +0200

    Add ActionButton and ActionButtonActivator

---
+++ Assets/Scripts/Action System/UI/ActionButton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButton : MonoBehaviour
+{
+    public Button button;
+    public Image icon;
+
+    private ActionContainer actionContainer;
+
+    private Unit unit;
+    private LocationTargetSelector locationTargetSelector;
+    private UnitTargetSelector unitTargetSelector;
+
+    private void Awake()
+    {
+        locationTargetSelector = FindObjectOfType<LocationTargetSelector>();
+        unitTargetSelector = FindObjectOfType<UnitTargetSelector>();
+    }
+
+    private void OnEnable()
+    {
+        button.onClick.AddListener(ActiveTargetSelection);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(ActiveTargetSelection);
+        actionContainer = null;
+    }
+
+
+    public void Activate(Unit unit,ActionContainer actionContainer)
+    {
+        this.unit = unit;
+        this.actionContainer = actionContainer;
+        button.interactable = unit.CanPerformeAction(this.actionContainer.action);
+        icon.sprite = this.actionContainer.action.icon;
+        gameObject.SetActive(true);
+    }
+
+    private void ActiveTargetSelection()
+    {
+        if(unit != null && actionContainer != null)
+        {
+            switch (actionContainer.action.targetType)
+            {
+                case Action.TargetType.Unit:
+                    break;
+                case Action.TargetType.Location:
+                    locationTargetSelector.Activate(unit, actionContainer);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Action System/UI/ActionButtonsActivator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButtonsActivator : MonoBehaviour
+{
+    public ActionButton prefab;
+    public GridLayoutGroup actionbuttonsLayout;
+    public int maxActionButtons = 10;
+    private ActionButton[] actionButtons;
+    private Selection selection;
+
+
+    private void Awake()
+    {
+        selection = FindObjectOfType<Selection>();
+    }
+
+    private void Start()
+    {
+        actionButtons = new ActionButton[maxActionButtons];
+
+        for (int i = 0; i < actionButtons.Length; i++)
+        {
+            actionButtons[i] = Instantiate(prefab, actionbuttonsLayout.transform);
+            actionButtons[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        selection.OnSelected.AddListener(ShowButtons);   
+        selection.OnDiselected.AddListener(HideButtons);   
+    }
+
+    private void OnDisable()
+    {
+        selection.OnSelected.RemoveListener(ShowButtons);
+        selection.OnDiselected.RemoveListener(HideButtons);
+    }
+
+    private void ShowButtons()
+    {
+        if(selection.HasSelection() && selection.selected.TryGetComponent(out Unit unit) && selection.selected.TryGetComponent(out ActionManager manager))
+        {
+            for (int i = 0; i < manager.actionContainers.Length; i++)
+            {
+                actionButtons[i].Activate(unit, manager.actionContainers[i]);
+            }
+        }
+    }
+
+    private void HideButtons()
+    {
+        for (int i = 0; i < actionButtons.Length; i++)
+        {
+            actionButtons[i].gameObject.SetActive(false);
+        }
+    }
+}
