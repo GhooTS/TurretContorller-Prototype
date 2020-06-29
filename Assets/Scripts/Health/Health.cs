ac57d70199f625eb1596f47b023a97e32f276507
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-29 19:19:06 +0200

    Add Health class from other project

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,68 @@
+﻿using GTVariable;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Health : MonoBehaviour
+{
+    [Tooltip("Whether or not take action when hp hit 0")]
+    public bool autoDestroy = true;
+    [Tooltip("If set to false game object will be disable")]
+    public bool destroyIfDie = true;
+    [SerializeField]
+    protected FloatReference HP;
+    public FloatReference MaxHP;
+    public bool Alive { get; private set; }
+
+    public UnityEvent damageTaken;
+    public UnityEvent died;
+
+    private void OnEnable()
+    {
+        HP.Value = MaxHP.Value;
+        Alive = true;
+    }
+
+    private void OnDisable()
+    {
+        Alive = false;
+    }
+
+    public virtual void DealDamage(float damage)
+    {
+        HP.Value -= damage;
+        damageTaken?.Invoke();
+        if (HP.Value <= 0)
+        {
+            Alive = false;
+            died?.Invoke();
+            if (autoDestroy)
+            {
+                Die();
+            }
+        }
+    }
+
+    public virtual float GetCurrentHP()
+    {
+        return HP.Value;
+    }
+
+    public virtual void Heal(float amount)
+    {
+        HP.Value = Mathf.Min(HP.Value + amount, MaxHP.Value);
+    }
+
+    public virtual void Die()
+    {
+        Alive = false;
+        if (destroyIfDie)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Health/UI/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    [SerializeField]
+    private Health health;
+
+
+    private void OnEnable()
+    {
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        bar.fillAmount = health.GetCurrentHP() / health.MaxHP.Value;
+    }
+}
