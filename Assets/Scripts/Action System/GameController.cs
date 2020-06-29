ecb25c970a9d0547488a19e121745c080e11ec66
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-29 17:19:40 +0200

    Add simple GameController

---
+++ Assets/Scripts/Action System/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public UnitQueueExecutor unitQueueExecutor;
+    public CameraController cameraController;
+
+    private void Update()
+    {
+        if (unitQueueExecutor.InCycle)
+        {
+            if (unitQueueExecutor.CurrentActionFinshed() && unitQueueExecutor.CurrentReactionFinshed())
+            {
+                unitQueueExecutor.EndActionOrReaction();
+                unitQueueExecutor.MoveToNext();
+                if (unitQueueExecutor.CurrentAction != null)
+                {
+                    StartCameraTransition();
+                }
+                else
+                {
+                    unitQueueExecutor.StartNextActionOrReaction();
+                }
+            }
+        }
+    }
+
+    private void StartCameraTransition()
+    {
+        StartCoroutine(WaitForCameraTransition());
+    }
+
+    private IEnumerator WaitForCameraTransition()
+    {
+        cameraController.StartTransition(unitQueueExecutor.CurrentActive.transform.position, unitQueueExecutor.CurrentAction.ActionTarget.targetLocation);
+        yield return new WaitForSeconds(cameraController.transitionTime);
+        unitQueueExecutor.StartNextActionOrReaction();
+    }
+}
