6cf35870ee57c234c43737214d4a509a42da208e
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-29 17:17:16 +0200

    Add HasStarted prop to QueuedAction

--- Assets/Scripts/Action System/Action/QueuedAction.cs
+++ Assets/Scripts/Action System/Action/QueuedAction.cs
@@ -4,11 +4,13 @@ public class QueuedAction
     public ActionController ActionController { get; set; }
     public Action Action { get; set; }
     public ActionTarget ActionTarget { get; set; }
+    public bool HasStarted { get; private set; } = false;
 
 
     public void Execute()
     {
         ActionController.Execute(Action, ActionTarget);
+        HasStarted = true;
     }
 
     public bool HasFinshed()
