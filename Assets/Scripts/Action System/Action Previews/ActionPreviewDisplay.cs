bb3f8c6ea303f112fd09ed49219eb8854737c5b4
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-30 14:03:21 +0200

    Add action preview display

---
+++ Assets/Scripts/Action System/Action Previews/ActionPreviewDisplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class ActionPreviewDisplay : MonoBehaviour
+{
+    public enum PreviewMode
+    {
+        Hidden,
+        Visible,
+        Focus,
+        Active
+    }
+    public PreviewMode Mode { get; protected set; }
+    public abstract void SetPreviewMode(PreviewMode mode);
+    public abstract void DestroyPreview();
+}
---
+++ Assets/Scripts/Action System/Action Previews/PreciseShotPreviewDisplay.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using GTVariable;
+
+public class PreciseShotPreviewDisplay : ActionPreviewDisplay
+{
+    public LineRenderer display;
+    [Header("Display Mode Colors")]
+    public GradientReference normal;
+    public GradientReference focus;
+    public GradientReference active;
+
+
+    public override void DestroyPreview()
+    {
+        Destroy(gameObject);
+    }
+
+    public override void SetPreviewMode(PreviewMode mode)
+    {
+        switch (mode)
+        {
+            case PreviewMode.Hidden:
+                display.enabled = false;
+                break;
+            case PreviewMode.Visible:
+                display.enabled = true;
+                display.colorGradient = normal.Value;
+                break;
+            case PreviewMode.Focus:
+                display.enabled = true;
+                display.colorGradient = focus.Value;
+                break;
+            case PreviewMode.Active:
+                display.enabled = true;
+                display.colorGradient = active.Value;
+                break;
+            default:
+                break;
+        }
+
+        Mode = mode;
+    }
+
+    public void SetDisplayTarget(Vector2 start,Vector2 target)
+    {
+        display.SetPosition(0, start);
+        display.SetPosition(1, target);
+    }
+}
