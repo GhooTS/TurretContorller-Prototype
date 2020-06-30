00ff9e57e1363ff58e89e768e12fbb6dbe5e4c53
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-06-30 15:35:50 +0200

    Change name from MultiShotPreview to AreaShotPreview and Correct some methods

---
+++ Assets/Scripts/Action System/Action Previews/AreaShotPreview.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Action System/View/AreaShot")]
+public class AreaShotPreview : ActionPreview
+{
+    public LayerMask layerMask;
+    public AreaShotPreviewDisplay display;
+    private AreaShotPreviewDisplay instance;
+    private ShootAction action;
+    private ShootActionController controller;
+
+
+    public override void Activate(Unit _, Action action, ActionController controller)
+    {
+        this.action = action as ShootAction;
+        this.controller = controller as ShootActionController;
+        instance = Instantiate(display,this.controller.rotationPoint.position,Quaternion.identity);
+        instance.SetDisplayTarget(Vector2.right, 0, this.action.range, this.action.bulletSpread);
+        instance.SetPreviewMode(ActionPreviewDisplay.PreviewMode.Focus);
+    }
+
+    public override void Clear()
+    {
+        Destroy(instance.gameObject);
+    }
+
+    public override void DetachPreview()
+    {
+        instance.SetPreviewMode(ActionPreviewDisplay.PreviewMode.Visible);
+        instance = null;
+    }
+
+    public override ActionPreviewDisplay GetPreviewDisplay()
+    {
+        return instance;
+    }
+
+    public override void UpdateView(Vector2 location)
+    {
+        instance.SetDisplayTarget((location - (Vector2)controller.rotationPoint.position).normalized, 0, action.range, action.bulletSpread);
+    }
+}
