94168a3a714b70d572fc4ab67925724eabcefdac
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-01 19:30:16 +0200

    Add particle projectile controller and damage particle

---
+++ Assets/Scripts/Projectiles/DamageParticle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class DamageParticle : MonoBehaviour
+{
+    public float damage;
+    private ParticleSystem particle;
+    private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+    private void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.TryGetComponent(out IDamageable damagable))
+        {
+            var amount = ParticlePhysicsExtensions.GetCollisionEvents(particle, other, collisionEvents);
+            damagable.TakeDamage(damage * amount);
+        }
+    }
+}
